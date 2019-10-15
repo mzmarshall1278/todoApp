6375441ae66068b9fc287e42dabd415ee56aebf0
Marshall <mzmarshall1278@gmail.com>
2019-10-15 17:05:16 +0100

    render a default message when there are no more todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mt-2 pt-5">
 
-<b-list-group>
-  <b-list-group-item v-for="(list, key) in lists" :key="key" @dblclick="remove(list.number)">{{list.text}}</b-list-group-item>
+<b-list-group >
+  <b-list-group-item  v-for="(list, key) in theList"  :key="key" @dblclick="remove(list.number)">{{list.text}}</b-list-group-item>
 
 </b-list-group>
 
@@ -15,6 +15,13 @@ export default {
   computed: {
     lists () {
       return this.$store.state.lists
+    },
+    theList () {
+      if (this.lists.length < 1) {
+        return [{ number: null, text: 'you dont have any more todos' }]
+      } else {
+        return this.lists
+      }
     }
   },
   methods: {
